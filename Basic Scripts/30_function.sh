2a5d5fd272471d13ea48c06134e12da042b9cc1f
nullChord7 <spandan993336@gmail.com>
2026-05-15 23:59:35 +0530

    30-31

---
+++ Basic Scripts/30_function.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#first way to define a function
+function welcomeNote() {
+	echo ".........."
+	echo "Welcome"
+	echo ".........."
+}
+
+#function call
+welcomeNote
+welcomeNote
+echo "\n"
+#second way of defining a funtion
+heyBuddy() {
+	echo ".........."
+    echo "Hey Buddy"
+    echo ".........."
+}
+
+#function call
+heyBuddy
+heyBuddy
---
+++ Basic Scripts/31_arguments.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+welcomeNote() {
+	echo "   .........."
+	echo "   Hello $1"
+	echo "   You're $2"
+	echo "   .........."
+
+}
+welcomeNote Alex 20
+welcomeNote nullChord7 23
